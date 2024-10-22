b7006cc7b64c7b3260b8c3377dbe91da0521c3d3
leandrovelosos <leandrovdsantos96@gmail.com>
2024-10-22 09:48:28 -0300

    get e post

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 import express from "express";
 
 const app = express();
+//mostra a express que usamos json
+app.use(express.json())
 
+const users = [];
+
+//salva os usuarios na variavel users
+app.post('/usuarios', (req, res) => {
+    
+    users.push(req.body)
+
+    res.send('Ok, post')
+});
+
+//responde com um json com todos os usuarios, rota de listagem 
 app.get('/usuarios', (req, res) => {
-            res.send('Ok, deu bom')
+            res.json(users)
+
 });
 
 app.listen(3000) 
