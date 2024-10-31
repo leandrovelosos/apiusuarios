296d9888980212a18bdd62ebe993116186022d64
leandrovelosos <leandrovdsantos96@gmail.com>
2024-10-31 18:32:45 -0300

    post e get

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express();
 //mostra ao express que usamos json
 app.use(express.json())
 
-const users = [];
 
 //salva os usuarios na variavel users
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
 
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
     res.status(201).json(req.body)
 });
 
 //responde com um json com todos os usuarios, rota de listagem 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
     res.status(200).json(users)
 
 });
