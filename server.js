69345664ed1047daad4af8231877e2c3cc34f269
leandrovelosos <leandrovdsantos96@gmail.com>
2024-10-31 19:44:37 -0300

    update e delete

--- server.js
+++ server.js
@@ -22,12 +22,56 @@ app.post('/usuarios', async (req, res) => {
     res.status(201).json(req.body)
 });
 
+
 //responde com um json com todos os usuarios, rota de listagem 
 app.get('/usuarios', async (req, res) => {
 
-    const users = await prisma.user.findMany()
+    let users = []
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                age: req.query.age,
+                email: req.query.email
+            }
+        })
+    }else{
+        users = await prisma.user.findMany()
+    }
+   
     res.status(200).json(users)
 
 });
 
+//update
+app.put('/usuarios/:id', async (req, res) => {
+
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+});
+
+
+//delete
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete ({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: 'Usuario deletado com sucesso'})
+})
 app.listen(3000) 
